a8d92914449e61dfe1a50c082b9d01d7da21c71a
Skylarce04 <edmon.alex@yahoo.com>
2026-01-29 22:26:10 +0700

    fix admin prerender issue lagiii

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic"
 import { Header } from "@/components/header"
 import { HeroSection } from "@/components/hero-section"
 import { ServiceTimes } from "@/components/service-times"
